c243001cf276b0d8aa8e107a9a573665342f9693
alfey504 <alfey504@gmail.com>
2024-02-02 22:11:34 -0500

    fixed bug on admin message page

--- src/app/admin/messages/actions.ts
+++ src/app/admin/messages/actions.ts
@@ -8,7 +8,7 @@ export const messageRead = async (id: number) => {
     try {
         const apiService = new ApiService() 
         const token = GetAuthToken()
-        const response  =  await apiService.put("admin/message", {id: id, read: true}, undefined, token)
+        const response  =  await apiService.put("admin/message", {} ,{id: id, read: true}, undefined, token)
         if(!response.ok){
             console.log(response)
             return false
